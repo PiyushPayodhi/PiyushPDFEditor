610a069a3dc0f063042aba0c15a103e0bf8aba68
Piyush Payodhi <ppayodhi@gmail.com>
2018-08-16 23:13:44 +0530

    A simple pdf editor implementation in a console application.

---
+++ PDFBoxEditor.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using org.apache.pdfbox.pdmodel;
+using org.apache.pdfbox.util;
+using org.apache.pdfbox.pdfparser;
+using java.io;
+using org.apache.pdfbox.pdmodel.encryption;
+using org.apache.pdfbox.pdmodel.interactive.form;
+
+namespace PiyushApps.PDFEditor
+{
+    class PDFBoxEditor
+    {
+        public string PdfFile { get; set; }
+        public string PdfPassword { get; set; }
+
+
+        public PDFBoxEditor(string pdfFile, string pdfPassword)
+        {
+            PdfFile = pdfFile;
+            PdfPassword = pdfPassword;
+        }
+
+        public string PdfToText()
+        {
+            string pdfText = String.Empty;
+            PDFParser parser = new PDFParser(new BufferedInputStream(new FileInputStream(PdfFile)));
+            parser.parse();
+            PDDocument originialPdfDoc = parser.getPDDocument();
+
+            bool isOriginalDocEncrypted = originialPdfDoc.isEncrypted();
+            if (isOriginalDocEncrypted)
+            {
+                originialPdfDoc.openProtection(new StandardDecryptionMaterial(PdfPassword));
+            }
+            PDFTextStripper stripper = new PDFTextStripper();
+            try
+            {
+                pdfText = stripper.getText(originialPdfDoc);
+
+            }
+            catch (java.io.IOException ex)
+            {
+                throw ex;
+            }
+            return pdfText;
+        }
+
+        public string PdfFields()
+        {
+            string pdfText = String.Empty;
+            PDFParser parser = new PDFParser(new BufferedInputStream(new FileInputStream(PdfFile)));
+            parser.parse();
+            PDDocument originialPdfDoc = parser.getPDDocument();
+
+            bool isOriginalDocEncrypted = originialPdfDoc.isEncrypted();
+            if (isOriginalDocEncrypted)
+            {
+                originialPdfDoc.openProtection(new StandardDecryptionMaterial(PdfPassword));
+            }
+            
+            try
+            {
+                PDDocumentCatalog docCatalog = originialPdfDoc.getDocumentCatalog();
+                PDAcroForm acroForm = docCatalog.getAcroForm();
+                PDField field = acroForm.getField("Name");
+                if (field != null)
+                {
+                    field.setValue("name");
+                }
+            }
+            catch (java.io.IOException ex)
+            {
+                throw ex;
+            }
+            return pdfText;
+        }
+
+    }
+}
---
+++ PDFEditorProgram.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using System.Security.Principal;
+
+namespace PiyushApps.PDFEditor
+{
+    class PDFEditorProgram
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+
+            string file = @"C:\Users\Pyuish\OneDrive\Documents\Personal\Finance\NSDL\NSDLe-CAS_110424803_2018_07.PDF";
+            string password = "BQPPP3943R";
+
+            //Console.WriteLine("Enter Document Path: ");
+            //file = Console.ReadLine();            
+            //Console.WriteLine("Enter Document Password:");
+            //password = Console.ReadLine();
+            
+            try
+            {
+                PDFBoxEditor pdfEditor = new PDFBoxEditor(file, password);
+                string pdfText = pdfEditor.PdfToText();
+                pdfEditor.PdfFields();
+                Logger.Info(pdfText);
+            }
+            catch(Exception ex)
+            {
+                Logger.Error(ex.Message);
+            }
+            
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+            
+        }
+    }
+}
